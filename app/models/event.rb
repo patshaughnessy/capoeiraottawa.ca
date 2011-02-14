2d7da56f509b3796a87f95f4bb11f2cd7a09077d
Pat Shaughnessy <pat@patshaughnessy.net>
2011-02-14 16:29:07 -0500

    removed attr_accessor for paperclip columns and added url option to has_attached_file

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,8 @@ class Event < ActiveRecord::Base
   geocoded_by :address, :latitude => :lat, :longitude => :lng
   after_validation :fetch_coordinates
 
-  attr_accessor :poster_thumb_file, :poster_file_name
-
-  has_attached_file :poster, :storage => :database, :styles => {:thumb => { :geometry => "100x100>", :column => 'poster_thumb_file'}}
+  has_attached_file :poster,
+                    :storage => :database,
+                    :styles => {:thumb => { :geometry => "100x100>", :column => 'poster_thumb_file'}},
+                    :url => '/:class/:id/:attachment?style=:style'
 end
